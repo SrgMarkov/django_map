497339066a78ff2eeaaadd409f7458fd5525effe
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2023-08-31 16:14:39 +0400

    Add PicturesInline to admin panel

--- places/admin.py
+++ places/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from .models import Location, Picture
 
 
+class PicturesInline(admin.TabularInline):
+    model = Picture
+
+
 @admin.register(Location)
-class UsersAdmin(admin.ModelAdmin):
+class Locations(admin.ModelAdmin):
+    inlines = [
+        PicturesInline,
+    ]
     list_display = ['title']
-
-
-@admin.register(Picture)
-class UsersAdmin(admin.ModelAdmin):
-    list_display = ['number', 'location']
