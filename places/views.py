d2cef833772554b0e0e0390260f884004e68959e
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2023-08-31 15:46:07 +0400

    Add json response to places path

--- places/views.py
+++ places/views.py
@@ -1,25 +1,40 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
 from django.template import loader
-from .models import Location
+from .models import Location, Picture
 
-features = []
-locations = Location.objects.all()
-for location in locations:
-    feature = {"type": "Feature", "geometry": {"type": "Point", "coordinates": [location.lng, location.lat]},
-               "properties": {"title": location.title,
-                              "placeId": location.id,
-                              "detailsUrl": f"./static/places/{location.id}.json"}}
-    features.append(feature)
+
+def get_features() -> dict:
+    features = []
+    locations = Location.objects.all()
+    for location in locations:
+        feature = {"type": "Feature", "geometry": {"type": "Point", "coordinates": [location.lng, location.lat]},
+                   "properties": {"title": location.title,
+                                  "placeId": location.id,
+                                  "detailsUrl": f"./static/places/{location.id}.json"}}
+        features.append(feature)
+    return {"type": "FeatureCollection", "features": features}
 
 
 def show_location_properties(request, location_id):
-    template = get_object_or_404(Location, id=location_id)
-    return HttpResponse(template.title)
+    location = get_object_or_404(Location, id=location_id)
+    images = Picture.objects.filter(location=location)
+    location_images = []
+    for image in images:
+        location_images.append(image.image.url)
+    location_description = {"title": location.title,
+                            "imgs": location_images,
+                            "description_short": location.description_short,
+                            "description_long": location.description_long,
+                            "coordinates": {
+                                "lng": location.lng,
+                                "lat": location.lat
+                            }}
+    return JsonResponse(location_description, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 1})
 
 
 def show_map(request):
     template = loader.get_template('index.html')
-    context = {'places_geo': {"type": "FeatureCollection", "features": features}}
+    context = {'places_geo': get_features()}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
