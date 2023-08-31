e53c42f31ae4d0ccdaeba72ad3c12a35d1171958
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2023-08-31 17:07:17 +0400

    Add preview image with resolution resize

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Location, Picture
 
 
 class PicturesInline(admin.TabularInline):
     model = Picture
+    readonly_fields = ['preview_image']
+
+    def preview_image(self, obj):
+        image_resolution = [obj.image.width, obj.image.height]
+        output_resolution = [0, 0]
+        output_resolution[image_resolution.index(max(image_resolution))] = 200
+        output_resolution[image_resolution.index(min(image_resolution))] = \
+            (min(image_resolution) * 200) / max(image_resolution)
+
+        return format_html('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.image.url,
+            width=output_resolution[0],
+            height=output_resolution[1]))
 
 
 @admin.register(Location)
@@ -12,3 +26,4 @@ class Locations(admin.ModelAdmin):
         PicturesInline,
     ]
     list_display = ['title']
+
