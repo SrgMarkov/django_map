aabacf9ad2bfc1802855a5999b6994e98b17504d
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2023-09-04 12:38:51 +0400

    Preparation to deploy

--- django_map/settings.py
+++ django_map/settings.py
@@ -11,23 +11,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3d=@%g)v)z_bhayl@ubunhe9xy$dtuza&fjw8x-17(r!%8!#bd'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == 1
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
 
-ALLOWED_HOSTS = []
-
-
+print(DEBUG)
 # Application definition
 
 INSTALLED_APPS = [
