47e17326f8e56695ecafc507758b8d452229eb38
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2023-09-05 13:10:02 +0400

    Add location title as unique with exception

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from django import db
 from django.core.management.base import BaseCommand
 from places.models import Location, Picture
 
@@ -16,19 +17,24 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         response = requests.get(options['download_url'])
+        response.raise_for_status()
         place_description = response.json()
-        Location.objects.get_or_create(title=place_description['title'],
-                                       description_short=place_description['description_short'],
-                                       description_long=place_description['description_long'],
-                                       lat=place_description['coordinates']['lat'],
-                                       lng=place_description['coordinates']['lng']
-                                       )
+        try:
+            Location.objects.get_or_create(title=place_description['title'],
+                                           description_short=place_description['description_short'],
+                                           description_long=place_description['description_long'],
+                                           lat=place_description['coordinates']['lat'],
+                                           lng=place_description['coordinates']['lng']
+                                           )
+        except db.IntegrityError:
+            print('This location already exists. Tittle must be unique')
         place_images = place_description['imgs']
         place = Location.objects.get(title=place_description['title'])
         for image in place_images:
             image_name = image.split('/')[-1]
             os.makedirs('media', exist_ok=True)
             image_response = requests.get(image, stream=True)
+            image_response.raise_for_status()
             with open(f'media/{image_name}', 'wb') as image_file:
                 for chunk in image_response.iter_content(chunk_size=512):
                     image_file.write(chunk)
--- places/models.py
+++ places/models.py
@@ -3,7 +3,7 @@ from tinymce.models import HTMLField
 
 
 class Location(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Название', null=True)
+    title = models.CharField(max_length=100, verbose_name='Название', null=True, unique=True)
     description_short = models.TextField(verbose_name='Краткое описание', null=True)
     description_long = HTMLField(verbose_name='Полное описание', null=True)
     lng = models.FloatField(verbose_name='Долгота')
