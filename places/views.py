3e359adf947d1757a4ea51e728c90a24bec9f141
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2023-09-06 22:04:27 +0400

    add comprehensions

--- places/views.py
+++ places/views.py
@@ -7,8 +7,7 @@ from .models import Location, Picture
 
 def get_features() -> dict:
     features = []
-    locations = Location.objects.all()
-    for location in locations:
+    for location in Location.objects.all():
         feature = {"type": "Feature", "geometry": {"type": "Point", "coordinates": [location.lng, location.lat]},
                    "properties": {"title": location.title,
                                   "placeId": location.id,
@@ -20,9 +19,7 @@ def get_features() -> dict:
 def show_location_properties(request, location_id):
     location = get_object_or_404(Location, id=location_id)
     images = Picture.objects.filter(location=location)
-    location_images = []
-    for image in images:
-        location_images.append(image.image.url)
+    location_images = [image.image.url for image in images]
     location_description = {"title": location.title,
                             "imgs": location_images,
                             "short_description": location.short_description,
