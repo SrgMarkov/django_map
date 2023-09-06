3d835ddc4aa90f952954433c945c08d61ba34383
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2023-09-06 19:24:49 +0400

    except None's, add blank

--- places/models.py
+++ places/models.py
@@ -3,11 +3,11 @@ from tinymce.models import HTMLField
 
 
 class Location(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Название', unique=True)
-    short_description = models.TextField(verbose_name='Краткое описание')
-    long_description = HTMLField(verbose_name='Полное описание')
-    lng = models.FloatField(verbose_name='Долгота')
-    lat = models.FloatField(verbose_name='Широта')
+    title = models.CharField(max_length=100, verbose_name='Название', unique=True, blank=True)
+    short_description = models.TextField(verbose_name='Краткое описание', blank=True)
+    long_description = HTMLField(verbose_name='Полное описание', blank=True)
+    lng = models.FloatField(verbose_name='Долгота', blank=True)
+    lat = models.FloatField(verbose_name='Широта', blank=True)
 
     def __str__(self):
         return self.title
@@ -17,7 +17,7 @@ class Picture(models.Model):
     number = models.AutoField(verbose_name='Номер картинки', primary_key=True, unique=True)
     location = models.ForeignKey(Location, verbose_name='Локация', related_name='image', on_delete=models.DO_NOTHING)
     image = models.ImageField(verbose_name='Изображение')
-    picture_order = models.PositiveIntegerField(verbose_name='Позиция', default=0)
+    picture_order = models.PositiveIntegerField(verbose_name='Позиция', default=0, blank=True)
 
     class Meta:
         ordering = ['picture_order']
