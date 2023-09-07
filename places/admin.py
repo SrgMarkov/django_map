7c9d9268e837a838ef3fd61f224d5e1a802be69d
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2023-09-07 11:30:31 +0400

    Add max-width, max-height

--- places/admin.py
+++ places/admin.py
@@ -15,17 +15,9 @@ class PicturesInline(SortableStackedInline):
     readonly_fields = ['preview_image']
 
     def preview_image(self, obj):
-        image_resolution = [obj.image.width, obj.image.height]
-        output_resolution = [0, 0]
-        output_resolution[image_resolution.index(max(image_resolution))] = 200
-        output_resolution[image_resolution.index(min(image_resolution))] = \
-            (min(image_resolution) * 200) / max(image_resolution)
-
-        return format_html('<img src="{url}" width="{width}" \
-                           height={height} />'.format(
-            url=obj.image.url,
-            width=output_resolution[0],
-            height=output_resolution[1]))
+        return format_html('<img src="{url}" style="max-width: 200px; \
+                            max-height: 200px"'.format(
+                            url=obj.image.url))
 
 
 @admin.register(Location)
